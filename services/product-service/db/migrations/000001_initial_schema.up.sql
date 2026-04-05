7076612eca02f482537be706943ef798a7adfa29
Vishal Shetagar <vishal.shetegar1@gmail.com>
2026-04-05 22:07:00 +0530

    chore: indexing on price and created_at

--- services/product-service/db/migrations/000001_initial_schema.up.sql
+++ services/product-service/db/migrations/000001_initial_schema.up.sql
@@ -11,3 +11,5 @@ CREATE TABLE IF NOT EXISTS products (
 );
 
 CREATE INDEX IF NOT EXISTS idx_products_category ON products(category);
+CREATE INDEX IF NOT EXISTS idx_products_price ON products(price);
+CREATE INDEX IF NOT EXISTS idx_products_created_at ON products(created_at);
